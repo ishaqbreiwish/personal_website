779ce404a694bc2bcabd0ac5abeb8fecd80f483e
Ishaq Breiwish <ibreiwish2022@uasdubai.ae>
2024-08-16 12:13:01 -0400

    vite commit

--- vite.config.mjs
+++ vite.config.mjs
@@ -1,6 +1,6 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import postcss from './postcss.config.js';
+import postcss from './postcss.config.mjs';
 
 export default defineConfig({
   plugins: [react()],
