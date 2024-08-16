72e2146465fdde98d50445c02bf1db467eaf2fdb
Ishaq Breiwish <ibreiwish2022@uasdubai.ae>
2024-08-16 12:11:46 -0400

    EMS Test

---
+++ postcss.config.mjs
@@ -0,0 +1,10 @@
+const tailwind = require('tailwindcss');
+const autoprefixer = require('autoprefixer');
+const tailwindConfig = require('./src/css/tailwind.config.mjs');
+
+export default {
+  plugins: {
+    tailwindcss: {},
+    autoprefixer: {},
+  }
+};
