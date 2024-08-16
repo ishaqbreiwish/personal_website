a0c46ffc2853700840dcd8a62c48830b96ad63ad
Ishaq Breiwish <ibreiwish2022@uasdubai.ae>
2024-08-15 23:12:20 -0400

    Simple vite for the purposes of testing vercel

--- vite.config.js
+++ vite.config.js
@@ -1,35 +1,6 @@
-import { defineConfig } from 'vite'
-import postcss from './postcss.config.js'
-import react from '@vitejs/plugin-react'
-import browserslist from 'browserslist';
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-
-// https://vitejs.dev/config/
 export default defineConfig({
-  define: {
-    'process.env': process.env
-  },
-  root: '.',
-  css: {
-    postcss,
-  },
   plugins: [react()],
-  resolve: {
-    alias: [
-      {
-        find: /^~.+/,
-        replacement: (val) => {
-          return val.replace(/^~/, "");
-        },
-      },
-    ],
-  },
-  build: {
-    commonjsOptions: {
-      transformMixedEsModules: true,
-    },
-    rollupOptions: {
-      external: ['react', 'react-dom'],
-    },
-  } 
-})
+});
